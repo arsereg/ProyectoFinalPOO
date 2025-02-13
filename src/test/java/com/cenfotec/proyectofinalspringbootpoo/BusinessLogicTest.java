aabc13a623ce6b48d834c4478fb52c88b5f7ae95
arsereg <arsereg@gmail.com>
2025-02-12 23:10:37 -0600

    Adding JUnit

---
+++ src/test/java/com/cenfotec/proyectofinalspringbootpoo/BusinessLogicTest.java
@@ -0,0 +1,15 @@
+package com.cenfotec.proyectofinalspringbootpoo;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class BusinessLogicTest extends TestCase {
+
+    @Test
+    public void testCrearTransaccion() {
+        assertEquals(1, 1);
+    }
+}
